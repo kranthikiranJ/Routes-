e576459c08d0dfd88fe0bacc6a91226c9c11e3ea
kranthikiranJ <kranthikiran1993@gmail.com>
2023-03-03 20:58:09 +0530

    Redirect added

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const Home = () => {
+  const [auth, setAuth] = useState(false);
+  if (auth) {
+    return <Navigate to="/dashboard" />;
+  }
   return (
     <div>
       <center>
         <h4>Displaying Home Page</h4>
+        <button onClick={() => setAuth(true)}>Login</button>
       </center>
     </div>
   );
